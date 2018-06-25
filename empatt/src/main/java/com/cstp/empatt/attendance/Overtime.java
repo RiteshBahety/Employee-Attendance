0f5c219c28d434164cce8f7fd2a780dd0d81588d
RiteshBahety <33376801+RiteshBahety@users.noreply.github.com>
2018-06-25 09:51:17 +0530

    Add files via upload

---
+++ empatt/src/main/java/com/cstp/empatt/attendance/Overtime.java
@@ -0,0 +1,18 @@
+package com.cstp.empatt.attendance;
+
+/**
+ * Number of hours worked extra beyond normal routine work.
+ *
+ * @author Ritesh Bahety on 6/25/2018.
+ */
+public class Overtime {
+    private int hours;
+
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+}
